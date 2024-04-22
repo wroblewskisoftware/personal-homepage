901290acca5f1352b5ee534bdcf35f74e9db6383
Radoslaw Wroblewski <wroblewski082@gmail.com>
2024-04-22 23:04:59 +0200

    preparation of the application skeleton for further coding

---
+++ src/globalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  html {
+  box-sizing: border-box;
+  }
+
+  *, ::after, ::before {
+  box-sizing: inherit;
+  }
+
+  body {
+  font-family: "Inter", sans-serif;
+  word-break: break-word;
+  }
+`;
+
+export default GlobalStyle;
