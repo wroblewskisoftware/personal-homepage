1bf5a9b9fdcd30dc04428d30bfa10538c17e8511
Radoslaw Wroblewski <wroblewski082@gmail.com>
2024-10-14 02:56:56 +0200

    style: adjust ThemeToggle component styling

--- src/Header/ThemeToggle/styled.js
+++ src/Header/ThemeToggle/styled.js
@@ -3,8 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   align-items: center;
-  width: 159px;
-  align-self: start;
+  justify-content: end;
 `;
 
 export const ToggleText = styled.span`
