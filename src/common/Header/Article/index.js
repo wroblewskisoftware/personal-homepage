e619bd1dd9ec894162bf63ada95dfca96ffd17fb
Radoslaw Wroblewski <wroblewski082@gmail.com>
2024-12-08 02:49:50 +0100

    update: modify personal bio in header section

--- src/common/Header/Article/index.js
+++ src/common/Header/Article/index.js
@@ -13,10 +13,12 @@ const Article = () => (
     <CaptionParagraph>THIS IS</CaptionParagraph>
     <StyledHeader>Radosław Wróblewski</StyledHeader>
     <LeadParagraph>
-      I am a Frontend Developer. I love using React. I am currently looking for
-      an opportunity to use my skills at work. I realize that I do not have
-      commercial experience, but I assure you that I make up for it with
-      commitment and willingness to learn.
+      I am a Frontend Developer passionate about React and building
+      user-friendly and visually appealing interfaces. For the last two years, I
+      have dedicated myself to developing my skills in technologies like React
+      (Redux, React Router, Redux Saga), JavaScript (ES6+, async/await), and CSS
+      (Flexbox, Grid, RWD). I’m excited to bring my fresh perspective to your
+      organization and deliver solutions that add real value.
     </LeadParagraph>
     <MessageLink
       href="mailto:wroblewskisoftware@gmail.com"
