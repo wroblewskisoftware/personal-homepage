9f554b9c12bc9d9be8a620cd95727e4ae804354c
Radoslaw Wroblewski <wroblewski082@gmail.com>
2024-12-21 16:35:17 +0100

    feat: create Portfolio component with Heading for desktop view

--- src/common/Main/Portfolio/index.js
+++ src/common/Main/Portfolio/index.js
@@ -1,11 +1,9 @@
 import { StyledArticle } from "./styled";
 import Heading from "./Heading";
-import LoadingPage from "./LoadingPage";
 
 const Portfolio = () => (
   <StyledArticle>
     <Heading />
-    <LoadingPage />
   </StyledArticle>
 );
 
