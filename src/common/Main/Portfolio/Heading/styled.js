da5680cfe3bacf7b47e515d6d95150443e18f380
Radoslaw Wroblewski <wroblewski082@gmail.com>
2024-12-22 23:39:42 +0100

    feat(Portfolio): add mobile styles for Heading component

--- src/common/Main/Portfolio/Heading/styled.js
+++ src/common/Main/Portfolio/Heading/styled.js
@@ -3,21 +3,38 @@ import { ReactComponent as GithubIcon } from "../../../../assets/githubLogo.svg"
 
 export const StyledContainer = styled.div`
   text-align: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    margin-top: -2px;
+  }
 `;
 
 export const StyledIcon = styled(GithubIcon)`
   height: 40px;
   margin-bottom: 12px;
   color: ${({ theme }) => theme.color.buttonBackground};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    height: 32px;
+  }
 `;
 
 export const StyledHeader = styled.h2`
   font-size: 30px;
   font-weight: 900;
   margin: 0 0 8px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 18px;
+    margin: 0 0 16px;
+  }
 `;
 
 export const StyledParagraph = styled.p`
   font-size: 20px;
   margin: 0 0 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    font-size: 17px;
+  }
 `;
