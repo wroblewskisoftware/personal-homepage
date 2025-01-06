8962d7e7c1ca437a0cc03d0353c31667264bee05
Radoslaw Wroblewski <wroblewski082@gmail.com>
2025-01-06 17:51:04 +0100

    chore: update authentication token

--- src/features/repo/apiCredentials.js
+++ src/features/repo/apiCredentials.js
@@ -1,4 +1,4 @@
 export const apiUrl = "https://api.github.com/users/wroblewskisoftware/repos";
 
 export const token =
-  "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";
+  "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";
