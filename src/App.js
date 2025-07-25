259ba6ba339b3e1e6dd66757f76f4147130bd2a5
Radoslaw Wroblewski <wroblewski082@gmail.com>
2025-07-25 02:31:06 +0200

    refactor: move GlobalStyle and ThemeProvider from index.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useSelector } from "react-redux";
+import { ThemeProvider } from "styled-components";
+import { selectTheme } from "./features/theme/themeSlice";
+import { lightTheme, darkTheme } from "./themes";
+import GlobalStyle from "./globalStyles";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
-const App = () => (
-  <>
-    <Header />
-    <Main />
-    <Footer />
-  </>
-);
+const App = () => {
+  const isDark = useSelector(selectTheme);
+
+  return (
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <GlobalStyle />
+      <Header />
+      <Main />
+      <Footer />
+    </ThemeProvider>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { ThemeProvider } from "./ThemeProvider";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import GlobalStyle from "./globalStyles";
 import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <App />
     </Provider>
   </React.StrictMode>
 );
